48d00e61bcb8b25885e0ba268125e1a78006952c
evansmunatsa <evansmunatsa7@gmail.com>
2024-07-26 10:44:09 +0200

    navigation fix

--- js/nav.js
+++ js/nav.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-    $(".navbar-toggler").click(function() {
-        $(".overlay").css("width", "100%");
-        $("#sidebar").css("width", "40vw");
-        localStorage.setItem('sidebarState', 'open');
-    });
-
-    $(".closebtn, .overlay").click(function() {
-        $(".overlay").css("width", "0");
-        $("#sidebar").css("width", "0");
-        localStorage.setItem('sidebarState', 'closed');
-    });
-
-    // Ensure sidebar state is preserved on page load
-    $(window).on('load', function() {
-        if (localStorage.getItem('sidebarState') === 'open') {
+        $(".navbar-toggler").click(function() {
             $(".overlay").css("width", "100%");
             $("#sidebar").css("width", "40vw");
-        } else {
+            localStorage.setItem('sidebarState', 'open');
+        });
+
+        $(".closebtn, .overlay").click(function() {
             $(".overlay").css("width", "0");
             $("#sidebar").css("width", "0");
-        }
-    });
+            localStorage.setItem('sidebarState', 'closed');
+        });
 
-    // Change logo and navbar background on scroll
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
-            $('.navbar').addClass('scrolled');
-        } else {
-            $('.navbar').removeClass('scrolled');
-        }
+        // Ensure sidebar state is preserved on page load
+        $(window).on('load', function() {
+            if (localStorage.getItem('sidebarState') === 'open') {
+                $(".overlay").css("width", "100%");
+                $("#sidebar").css("width", "40vw");
+            } else {
+                $(".overlay").css("width", "0");
+                $("#sidebar").css("width", "0");
+            }
+        });
+
+        // Change logo and navbar background on scroll
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 50) {
+                $('.navbar').addClass('scrolled');
+            } else {
+                $('.navbar').removeClass('scrolled');
+            }
+        });
     });
-});
